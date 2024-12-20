0a7b7b871e2e57dcd3098fb1b7c4e32fbec11265
Gerd Braun <webmaster@your-d-sign.de>
2024-12-20 11:00:27 +0100

    small fix

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import { login, getProfile, register } from "../controllers/userController.js";
 import { authenticate } from "../middlewares/authenticate.js";
 import { validateUser } from "../middlewares/validateRequest.js";
+import { recoverPw, resetPw } from "../controllers/resetPwController.js";
 
 const authRouter = express.Router();
 
@@ -9,10 +10,7 @@ authRouter.post("/signup", validateUser, register);
 authRouter.post("/login", validateUser, login);
 authRouter.get("/profile", authenticate, getProfile);
 
-<<<<<<< Updated upstream
-=======
 authRouter.post("/recover-password", recoverPw);
 authRouter.post("/reset-password", resetPw);
->>>>>>> Stashed changes
 
 export default authRouter;
