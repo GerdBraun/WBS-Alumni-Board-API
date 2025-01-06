e8e7cb644b83eec0c8124c6a1315d57afe0e3dbd
Hema Bindra <hbindra11@yahoo.com>
2025-01-06 14:37:12 +0100

    clouduploader bug fix - middleware only processes a multer file if it exists

--- middlewares/cloudUploader.js
+++ middlewares/cloudUploader.js
@@ -1,6 +1,6 @@
 import { Buffer } from "node:buffer";
 import { v2 as cloudinary } from "cloudinary";
-import { ErrorResponse } from "../utils/ErrorResponse.js";
+//import { ErrorResponse } from "../utils/ErrorResponse.js";
 
 // Configuration
 cloudinary.config({
@@ -16,14 +16,17 @@ const cloudUploader = async (req, res, next) => {
   //"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
 
   try {
-    if (!req.file) throw new ErrorResponse("please upload a file", 400);
-    const b64 = Buffer.from(req.file.buffer).toString("base64");
-    const dataURI = "data:" + req.file.mimetype + ";base64," + b64;
-    const cloudinaryData = await cloudinary.uploader.upload(dataURI, {
-      resource_type: "auto",
-    });
+    //if (!req.file) throw new ErrorResponse("please upload a file", 400);
+    //only converts if there is a file to upload at all
+    if (req.file) {
+      const b64 = Buffer.from(req.file.buffer).toString("base64");
+      const dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+      const cloudinaryData = await cloudinary.uploader.upload(dataURI, {
+        resource_type: "auto",
+      });
 
-    req.cloudinaryURL = cloudinaryData.secure_url;
+      req.cloudinaryURL = cloudinaryData.secure_url;
+    }
     next();
   } catch (error) {
     next(error);
